e0f9745396f7e181f3856cd7f3ec0917d1b3d3f8
Davidmg97 <garciamotatodavid@gmail.com>
2026-05-23 11:22:56 -0500

    CRUD  completo funcionando con bootstrap

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, delete_task, toggle_complete, edit_task
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path(
+        'delete/<int:task_id>/',
+        delete_task,
+        name='delete_task'
+    ),
+    path('complete/<int:task_id>/',
+         toggle_complete,
+         name='toggle_complete'
+    ),
+    path('edit/<int:task_id>/',
+         edit_task,
+         name='edit_task'
+    ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -14,3 +14,33 @@ def task_list(request):
     
     return render(request, 'tasks/task_list.html', context)
 
+def delete_task(request, task_id):
+    
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    
+    return redirect('task_list')
+
+def toggle_complete(request, task_id):
+    
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    
+    return redirect('task_list')
+
+def edit_task(request, task_id):
+    
+    task = Task.objects.get(id=task_id)
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        task.title = title
+        task.save()
+        
+        return redirect('task_list')
+    
+    context = {'task': task}
+    
+    return render(request, 'tasks/edit_task.html', context)
+
